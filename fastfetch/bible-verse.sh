c38d4bec4e93d2d5d4fab8e922363c70ca694921
Steven Andraina Randrianasolo <sterandr@c1r12s11.42antananarivo.mg>
2026-08-19 14:01:11 +0300

    fix: remove frame and center bible verse

--- fastfetch/bible-verse.sh
+++ fastfetch/bible-verse.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 verse=$(curl -s "https://labs.bible.org/api/?passage=random&formatting=plain" 2>/dev/null | head -c 100)
 if [ -n "$verse" ]; then
-    len=${#verse}
-    border=""
-    i=0
-    while [ $i -lt $((len + 4)) ]; do
-        border="${border}─"
-        i=$((i + 1))
-    done
-    echo "╭${border}╮"
-    printf "│ \033[3;37m${verse}\033[0m │\n"
-    echo "╰${border}╯"
+    printf "%*s\n" $(( ($(tput cols) + ${#verse}) / 2 )) "$verse"
 fi
